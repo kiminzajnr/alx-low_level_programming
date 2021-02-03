929c3caffc3defd534a399fa3e014b151a6a8b76
kiminzajnr <kiminzaeric@gmail.com>
2021-02-03 18:06:01 +0000

    print to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+   * main - prints to the standard error
+   *
+   * Return: one
+   */
+main()
+{
+	fprintf(stderr, "and that piece of art is useful
+			\" - Dora Korpar,2015-10-19\n");
+	return (1);
+}
