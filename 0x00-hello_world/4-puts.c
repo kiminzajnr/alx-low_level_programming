aca34298d0232db1ad2211a74aca2112fe526199
kiminzajnr <kiminzaeric@gmail.com>
2021-02-03 14:33:48 +0000

    c program to print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+	char Mystr = "\"Programming is like building a multilingual puzzle";
 
 	puts(Mystr);
 	return (0);
