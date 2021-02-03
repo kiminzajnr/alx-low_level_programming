4d6bd49f74ac47305aa11b7cbc612bc3bd418bb5
kiminzajnr <kiminzaeric@gmail.com>
2021-02-03 15:54:01 +0000

    use printf to print onto screen

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+   * main - Prints onto screen
+   *
+   * Return: zero
+   */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
