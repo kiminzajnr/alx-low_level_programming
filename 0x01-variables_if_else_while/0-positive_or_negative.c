23122947d6023f0a32173750058228d1eddfaef4
kiminzajnr <kiminzaeric@gmail.com>
2021-02-04 12:28:46 +0000

    check whether zero, positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+   * main - checks if positive, zero or negative
+   *
+   *Return: zero
+   */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	} else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	} else
+	{
+		printf("%d is negative\n", n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Variabe, if else, while, for, do while, keywords, logical operators, arithmetic operators etc in c programming
