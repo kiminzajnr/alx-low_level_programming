b88b28411c05c3af731e69000831e1db0d2a502a
Erick Kiminza <kiminzaeric@gmail.com>
2021-02-05 22:33:00 +0300

    print 00 to 99 without varchar

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -11,8 +11,8 @@ int main(void)
 
 	for (i = 0; i <= 99; i++)
 	{
-		putchar((i/10) + '0');
-		putchar((i%10) + '0');
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
 		if (i == 99)
 			break;
 		putchar(',');		
