f193da2bb3f1b38fb5477252339aed33be7fae3a
Erick Kiminza <kiminzaeric@gmail.com>
2021-02-09 12:32:38 +0300

    check sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_sign - check sign of a number.
+ *
+ * @n: number to be checked
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
