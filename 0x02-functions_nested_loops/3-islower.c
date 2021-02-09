091b10d89c724b0d7b25c1763e80f97ec5fec937
Erick Kiminza <kiminzaeric@gmail.com>
2021-02-09 12:02:25 +0300

    check if lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z' )
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
