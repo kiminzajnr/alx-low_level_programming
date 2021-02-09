ef8ed4af1ee5942e9e51be74f0862506c31a1430
Erick Kiminza <kiminzaeric@gmail.com>
2021-02-09 23:09:24 +0300

    debugging testcase for zero

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "holberton.h"
+
 /**
-* positive_or_negative - for debugging
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-int positive_or_negative(void)
-{
-	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n == 0)
-		printf("%d is zero", n);
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
 	return (0);
 }
