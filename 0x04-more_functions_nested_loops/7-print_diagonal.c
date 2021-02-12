ce3a0a036ae51315756bbac0e78a439548642338
Erick Kiminza <kiminzaeric@gmail.com>
2021-02-12 16:57:55 +0300

    print diagnal line on terminal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,19 +11,15 @@ void print_diagonal(int n)
 
 	for (i = 1; i <= n; i++)
 	{
-		for (j = 1; j <= n; j++)
+		for (j = 1; j <= i; j++)
 		{
-			if (j == i)
+			if (i >= 2 && j <= i - 1)
 			{
-				_putchar('\\');
-			}
-			else if (j == n)
-			{
-				break;
+				_putchar(' ');
 			}
 			else
 			{
-				_putchar(' ');
+				_putchar('\\');
 			}
 		}
 		if (i == n)
