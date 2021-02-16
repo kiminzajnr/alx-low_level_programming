32d89c1b9a0e59f5eff3392589d263282f52db18
Erick Kiminza <kiminzaeric@gmail.com>
2021-02-16 22:10:14 +0300

    swaps values of two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,10 +4,12 @@
  * swap_int - swaps the values of two integers.
  *
  * @a: first integer.
- * @b: second integer
+ * @b: second integer.
  */
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
+	int t = a;
+
+	*a = *b;
+	*b = t;
 }
