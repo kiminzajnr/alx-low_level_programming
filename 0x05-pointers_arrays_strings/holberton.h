f072f38e642fdf44ddd75f6a297dea6eb9d908bc
Erick Kiminza <kiminzaeric@gmail.com>
2021-02-17 12:03:43 +0300

    header file

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -3,3 +3,7 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+void print_rev(char *s);
+void puts2(char *str);
+void rev_string(char *s);
+int string_length(char*);
