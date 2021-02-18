f3c345e833354d357e6d60b4119e59be730ef4ea
Erick Kiminza <kiminzaeric@gmail.com>
2021-02-18 19:38:29 +0300

    header file

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -4,3 +4,4 @@ int string_length(char *pointer);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
