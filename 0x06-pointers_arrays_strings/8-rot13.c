b133410d226a32f16463c647dd2880823e3a875f
Erick Kiminza <kiminzaeric@gmail.com>
2021-02-19 11:21:58 +0300

    encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+/**
+ * *rot13 - encodes a string using rot13.
+ *
+ * @str: string.
+ * Return: string.
+ */
+char *rot13(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((*(str + i) >= 'a' && *(str + i) < 'n')
+				|| (*(str + i) >= 'A' && *(str + i) < 'N'))
+		{
+			*(str + i) += 13;
+		}
+		else if ((*(str + i) >= 'n' && *(str + i) <= 'z')
+				|| (*(str + i) >= 'N' && *(str + i) <= 'Z'))
+		{
+			*(str + i) -= 13;
+		}
+	}
+	return (str);
+}
