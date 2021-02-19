6f2220fa5b35a99755cb64a8babcc731781ea249
Erick Kiminza <kiminzaeric@gmail.com>
2021-02-19 08:28:50 +0300

    reverses the content of an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,15 +9,12 @@
  */
 void reverse_array(int *a, int n)
 {
-	int b[n];
-	int i;
+	int temp;
 
-	for (i = 0; i < n; i++)
+	for (int low = 0, high = n - 1; low < high; low++, high--)
 	{
-		b[n - 1 - i] = a[i];
-	}
-	for (i = 0; i < n; i++)
-	{
-		a[i] = b[i];
+		temp = a[low];
+		a[low] = a[high];
+		a[high] = temp;
 	}
 }
