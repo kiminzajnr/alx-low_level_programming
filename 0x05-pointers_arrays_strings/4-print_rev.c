600c990ed26b7387e71929fcba578c5d86e49059
Erick Kiminza <kiminzaeric@gmail.com>
2021-02-20 16:09:44 +0300

    prints string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string in reverse.
+ *
+ * @s: store the string.
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = string_length(s);
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
+
+/**
+  * string_length - finds the length of a string.
+  * Return: length of c.
+  * @pointer: pointer.
+  */
+int string_length(char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
