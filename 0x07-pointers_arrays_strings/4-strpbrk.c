9ae97f9a4aa75f93c36faad8018f49b5c38b1485
Erick Kiminza <kiminzaeric@gmail.com>
2021-02-23 12:01:42 +0300

    searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,23 +10,31 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	char *stringChars = NULL;
-
 	if (s == NULL || accept == NULL)
 	{
 		return (NULL);
 	}
 
-	while (*s != '\0')
+	while (*s)
 	{
-		stringChars = accept;
-		while (*stringChars != '\0')
-		{
-			if (*s == *stringChars)
-				return (s);
-			stringChars++;
-		}
+		if (_strchr(accept, *s))
+			return (s);
 		s++;
 	}
-	return (s);
+	return (NULL);
+}
+/**
+ * *_strchr - locates a character in a string.
+ *
+ * @s: string.
+ * @c: character to be located.
+ * Return: pointer to first occurrence of NULL if no character found.
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++);
+	return (NULL);
 }
