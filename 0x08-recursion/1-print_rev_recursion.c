edadad0e5375475c48e8c8ac0e64010dba3829bf
Erick Kiminza <kiminzaeric@gmail.com>
2021-02-24 13:06:30 +0300

    prints a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ *
+ *@s: string
+ * Return: nothing.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
