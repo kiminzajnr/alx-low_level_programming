86db4e822e77e2e286bda4cf31f02ae760e83c43
Erick Kiminza <kiminzaeric@gmail.com>
2021-02-25 12:56:58 +0300

    header file

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -4,3 +4,7 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int isqrt(int n, int i);
