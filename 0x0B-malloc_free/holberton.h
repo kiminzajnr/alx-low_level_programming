ec126a6b18edb7de7d50c4ce7bde96f4d0446116
Erick Kiminza <kiminzaeric@gmail.com>
2021-03-03 15:47:53 +0300

    header file

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -5,4 +5,5 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 int string_length(char *pointer);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 #endif
