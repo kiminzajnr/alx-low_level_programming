61cec6ecd631a5bc7f17beceec578bace1204df2
Erick Kiminza <kiminzaeric@gmail.com>
2021-03-04 14:19:02 +0300

    header file

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,4 +1,6 @@
 #ifndef FILE_HOLBERTON_H
 #define FILE_HOLBERTON_H
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+int string_length(char *pointer);
 #endif
