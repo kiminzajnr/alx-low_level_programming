b3d7487225bdd8395aff06a4a73c60486b25a93d
Erick Kiminza <kiminzaeric@gmail.com>
2021-03-07 16:20:37 +0300

    prints an integer

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints an interger.
+ * @n: interger.
+ * Return: nothing.
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar('0' + n % 10);
+	if (n == 0)
+		_putchar('0');
+}
