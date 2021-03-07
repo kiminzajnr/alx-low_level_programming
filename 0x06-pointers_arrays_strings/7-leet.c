e07a3db2dde3336400de27932292c32262df48f4
Erick Kiminza <kiminzaeric@gmail.com>
2021-03-07 13:15:19 +0300

    encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * *leet - check the code for Holberton School students.
+ *
+ * @str: string.
+ * Return: string.
+ */
+char *leet(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+		{
+			str[i] = '4';
+		}
+		else if (str[i] == 'e' || str[i] == 'E')
+		{
+			str[i] = '3';
+		}
+		else if (str[i] == 'o' || str[i] == 'O')
+		{
+			str[i] = '0';
+		}
+		else if (str[i] == 't' || str[i] == 'T')
+		{
+			str[i] = '7';
+		}
+		else if (str[i] == 'l' || str[i] == 'L')
+		{
+			str[i] = '1';
+		}
+	}
+	return (str);
+}
