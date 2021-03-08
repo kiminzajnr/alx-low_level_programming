b702dfc2107ff65c94131fc3b69f4158cc349b5a
Erick Kiminza <kiminzaeric@gmail.com>
2021-03-08 20:01:57 +0300

    initializes variable of ty struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,7 +11,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 }
