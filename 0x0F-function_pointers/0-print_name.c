6df82067417b95d08c33be24043fe94cb3c2f132
Erick Kiminza <kiminzaeric@gmail.com>
2021-03-10 14:19:58 +0300

    prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: pointer to function f.
+ * @f
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
