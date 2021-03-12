fbe3ab0bc461dae71d97316c1822f74a311f4dff
Erick Kiminza <kiminzaeric@gmail.com>
2021-03-12 16:54:39 +0300

    the main function

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+include "3-calc.h"
+
+/**
+  * main - program entry.
+  * @argc: argument counter.
+  * @argv: argument vector
+  * Return: 0
+  */
+int main(argc, *argv[])
+{
+	int a = 0, b = 0, result = 0;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	s = argv[2][0];
+
+	if (s != '+' && s != '-' && s != '*' && s != '/' && s != '%')
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	result = (get_op_func(argv[2]))(a, b);
+	printf("%d\n", result);
+	return (0);
+}
