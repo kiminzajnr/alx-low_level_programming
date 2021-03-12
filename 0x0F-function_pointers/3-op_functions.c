a8d1d7f3b065c8df78a54b25e132c1ca3da515d1
Erick Kiminza <kiminzaeric@gmail.com>
2021-03-12 16:53:23 +0300

    5 functions to perform the operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+  * op_add - adds two numbers
+  * @a: first number.
+  * @b: second number.
+  * Return: a + b.
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * op_sub - difference of two numbers
+  * @a: first number.
+  * @b: second number.
+  * Return: a - b.
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * op_mul - multiplies two numbers
+  * @a: first number.
+  * @b: second number.
+  * Return: a * b.
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * op_div - divides two numbers
+  * @a: first number.
+  * @b: second number.
+  * Return: a / b.
+  */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		print("Error\n");
+		exit(100);
+	return (a / b);
+}
+
+/**
+  * op_mod - finds modulus of two numbers
+  * @a: first number.
+  * @b: second number.
+  * Return: a % b.
+  */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
+
