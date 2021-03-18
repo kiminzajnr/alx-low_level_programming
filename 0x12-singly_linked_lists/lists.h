123bd482411e1ba06b085bdda5d4e71a4867e6fc
Erick Kiminza <kiminzaeric@gmail.com>
2021-03-18 20:02:26 +0300

    added prototype for free list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,4 +21,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 int string_length(const char *pointer);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 #endif
