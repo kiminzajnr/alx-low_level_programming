5be96c62e852a7a1d1299280bb01d382109a8c0b
Erick Kiminza <kiminzaeric@gmail.com>
2021-03-20 10:55:24 +0300

    frees a linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -12,9 +12,9 @@ void free_list(list_t *head)
 
 	while (head != NULL)
 	{
-		temp = head;
-		head = head->next;
-		free(temp->str);
-		free(temp);
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
 	}
 }
