45e649a29ad7a63df1ecddfde4bde4a3b4b86304
Erick Kiminza <kiminzaeric@gmail.com>
2021-03-24 17:57:59 +0300

    insert node at a position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	for (i = 0; i < idx - 1; i++)
 	{
 		temp = temp->next;
+		if (temp == NULL)
+		{
+			return (NULL);
+		}
 	}
 	newnode->next = temp->next;
 	temp->next = newnode;
