1c43ac36f34c68e748495feb8a0e3f87a89094ac
Erick Kiminza <kiminzaeric@gmail.com>
2021-05-02 23:15:39 +0300

    add node at certain index of doubly linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	newnode->prev = NULL;
 	if (idx == 0)
 	{
-		newnode = *h;
+		*h = newnode;
 		return (newnode);
 	}
 	temp = *h;
@@ -32,6 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 	}
 	newnode->next = temp->next;
+	temp->next->prev = newnode;
 	temp->next = newnode;
 	newnode->prev = temp;
 
