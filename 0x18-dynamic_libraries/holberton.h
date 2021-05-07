b5917e9fee019f2ac41245b4648e92025835665e
Erick Kiminza <kiminzaeric@gmail.com>
2021-05-07 15:03:37 +0300

    updated header file

--- 0x18-dynamic_libraries/holberton.h
+++ 0x18-dynamic_libraries/holberton.h
@@ -1,5 +1,5 @@
 #ifndef HOLBERTON_H
-#define HOLBERTOn_H
+#define HOLBERTON_H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,4 +20,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int string_length(char *pointer);
 #endif
