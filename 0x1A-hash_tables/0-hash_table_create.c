215a96ef6c51d6954353411a0edfe879c75b08c6
Erick Kiminza <kiminzaeric@gmail.com>
2021-05-28 09:28:01 +0300

    if size < 0, return NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,6 +11,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 
+	if (size < 1)
+		return (NULL);
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
