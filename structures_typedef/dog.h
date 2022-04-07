aafbad00d9846ec9a3718cd00a3893ad86150cdf
kiminzajnr <kiminzaeric@gmail.com>
2022-04-07 12:28:34 +0300

    testing struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+# ifndef DOG_H
+# define DOG_H
+/**
+ * struct dog - dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Description: structure is a user defined data
+ * type in c that allows different data items to be combined.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+# endif
