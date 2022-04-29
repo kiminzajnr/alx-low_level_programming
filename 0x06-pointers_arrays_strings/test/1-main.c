52824cdee84e8f7d350ebccf22bfd24b5fa889fd
kiminzajnr <kiminzaeric@gmail.com>
2022-04-29 13:53:39 +0300

    address-of

---
+++ 0x06-pointers_arrays_strings/test/1-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - addresses of variables
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	char c;
+	int n;
+
+	printf("Address of variable 'c': %p\n", &c);
+	printf("Address of variable 'n': %p\n", &n);
+	return (0);
+}
