0d3f4962666bd9f5fc8c3625cec8e2f22dcfcd8b
kiminzajnr <kiminzaeric@gmail.com>
2022-04-29 14:09:18 +0300

    address where a pointer is stored

---
+++ 0x06-pointers_arrays_strings/test/3-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - printing the address of a pointer
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	int *p;
+	printf("Address of variable 'p': %p\n", &p);
+	return (0);
+}
