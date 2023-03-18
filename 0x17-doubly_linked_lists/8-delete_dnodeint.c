64723e7846a4b62e3087e12257b2a694996df25e
kiminzajnr <kiminzaeric@gmail.com>
2023-03-18 22:58:27 +0300

    fix delete node

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -18,6 +18,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = temp->next;
+		temp->next = NULL;
+		temp->prev = NULL;
 		free(temp);
 		return (1);
 	}
